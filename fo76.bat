b2fd3c123fa506ff4a2daa01dd20ba527474c868
Ragingunlcorn <84747168+Ragingunlcorn@users.noreply.github.com>
2021-05-24 17:18:59 -0400

    Update fo76.bat

--- fo76.bat
+++ fo76.bat
@@ -1,4 +1,4 @@
 @echo off
-xcopy /i "D:\Program Files\ModifiableWindowsApps\Fallout 76\Project76.ini" "C:\Users\MYUSERNAME\OneDrive\Documents\My Games\Fallout 76\" /Y
-attrib +R "C:\Users\MYUSERNAME\OneDrive\Documents\My Games\Fallout 76\Project76.ini"
-start "" "C:\Users\MYUSERNAME\OneDrive\Desktop\Fallout 76.lnk"
+xcopy /i "C:\Program Files\ModifiableWindowsApps\Fallout 76\Project76.ini" "C:\Users\hjk iui\OneDrive\Documents\My Games\Fallout 76\" /Y
+attrib +R "C:\Users\hjk iui\OneDrive\Documents\My Games\Fallout 76\Project76.ini"
+start "" "C:\Users\hjk iui\OneDrive\Desktop\Fallout 76.lnk"
